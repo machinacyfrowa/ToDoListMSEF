cd8202d2918a951143e4da010193eb12bc073434
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-11-17 10:15:10 +0100

    Dodano nowe kontrolki i logikę do formularza ToDo
    Zaktualizowano `Form1.Designer.cs`, aby dodać kontrolki: `TextBox`, `Label`, `DateTimePicker` i `Button`. Usunięto zbędne przypisania.
    
    Dodano metodę `AddToDoButton_Click` w `Form1.cs`, która obsługuje dodawanie zadań do bazy danych oraz czyszczenie `TextBox` po dodaniu.
    
    Wprowadzono zmiany w pliku `Form1.resx`, poprawiając nagłówki i formatowanie.
    
    Zaktualizowano `ToDoList.db`, co sugeruje zmiany w strukturze bazy danych.

--- ToDoListMSEF/Form1.Designer.cs
+++ ToDoListMSEF/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            ToDoNameTextBox = new TextBox();
+            label1 = new Label();
+            ToDoDateTimePicker = new DateTimePicker();
+            AddToDoButton = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // ToDoNameTextBox
+            // 
+            ToDoNameTextBox.Location = new Point(29, 47);
+            ToDoNameTextBox.Name = "ToDoNameTextBox";
+            ToDoNameTextBox.Size = new Size(334, 23);
+            ToDoNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Zadanie od wykonania:";
+            // 
+            // ToDoDateTimePicker
+            // 
+            ToDoDateTimePicker.Format = DateTimePickerFormat.Short;
+            ToDoDateTimePicker.Location = new Point(369, 47);
+            ToDoDateTimePicker.Name = "ToDoDateTimePicker";
+            ToDoDateTimePicker.Size = new Size(200, 23);
+            ToDoDateTimePicker.TabIndex = 2;
+            // 
+            // AddToDoButton
+            // 
+            AddToDoButton.Location = new Point(575, 46);
+            AddToDoButton.Name = "AddToDoButton";
+            AddToDoButton.Size = new Size(75, 23);
+            AddToDoButton.TabIndex = 3;
+            AddToDoButton.Text = "Dodaj";
+            AddToDoButton.UseVisualStyleBackColor = true;
+            AddToDoButton.Click += AddToDoButton_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(369, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Termin wykonania:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(AddToDoButton);
+            Controls.Add(ToDoDateTimePicker);
+            Controls.Add(label1);
+            Controls.Add(ToDoNameTextBox);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox ToDoNameTextBox;
+        private Label label1;
+        private DateTimePicker ToDoDateTimePicker;
+        private Button AddToDoButton;
+        private Label label2;
     }
 }
--- ToDoListMSEF/Form1.cs
+++ ToDoListMSEF/Form1.cs
@@ -6,5 +6,32 @@ namespace ToDoListMSEF
         {
             InitializeComponent();
         }
+
+        private void AddToDoButton_Click(object sender, EventArgs e)
+        {
+            //pobiermay nazwê zadania z textboxa
+            string name = ToDoNameTextBox.Text;
+            //pobieramy datê wykonania z DateTimePickera
+            DateTime dueDate = ToDoDateTimePicker.Value;
+            //tworzymy obiekt dostêpu do bazy danych
+            Database db = new Database();
+            //sprawdzamy czy baza istnieje, jeœli nie to j¹ tworzymy
+            db.Database.EnsureCreated();
+            //tworzymy nowe zadanie
+            ToDoItem newItem = new ToDoItem();
+            //nadajemy mu nazwe, datê utworzenia i datê wykonania
+            newItem.Name = name;
+            newItem.CreatedAt = DateTime.Now;
+            newItem.DueDate = dueDate;
+            newItem.IsCompleted = false;
+            //dodajemy zadanie do bazy
+            db.ToDoItems.Add(newItem);
+            //zapisujemy zmiany w bazie
+            db.SaveChanges();
+            //czyœcimy textboxa
+            ToDoNameTextBox.Text = "";
+            //wyœwietlamy komunikat
+            MessageBox.Show("Zadanie dodane pomyœlnie!");
+        }
     }
 }
