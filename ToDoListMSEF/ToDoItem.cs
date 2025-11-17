42b7a34b91c810824053d8a2b6678fc47477f93c
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-11-17 08:32:02 +0100

    Dodano klasę ToDoItem do zarządzania zadaniami
    Wprowadzono nową klasę `ToDoItem`, która opisuje zadanie do wykonania. Klasa zawiera właściwości: `Id`, `Name`, `CreatedAt`, `DueDate` oraz `IsCompleted`, przechowujące istotne informacje o zadaniu. Dodano również przestrzenie nazw oraz dokumentację XML dla lepszego zrozumienia funkcjonalności klasy.

---
+++ ToDoListMSEF/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListMSEF
+{
+    /// <summary>
+    /// Klasa opisuje jedno z zadań do wykonania
+    /// Klasa zawiera właściwości opisujące zadanie: nazwa, data utworzenia, termin wykonania,
+    /// oraz status wykonania (zrobione/nie zrobione).
+    /// </summary>
+    internal class ToDoItem
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime DueDate { get; set; }
+        bool IsCompleted { get; set; }
+    }
+}
